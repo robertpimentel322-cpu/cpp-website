4134c9d4b963f0b54ecc51645788991c793f398c
robertpimentel322-cpu <robertpimentel322@gmail.com>
2025-12-20 00:20:59 +0100

    Create index.cpp

---
+++ index.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Content-Type: text/html\n\n";
+
+    cout << "<!DOCTYPE html>";
+    cout << "<html>";
+    cout << "<head>";
+    cout << "<title>C++ Website</title>";
+    cout << "</head>";
+    cout << "<body style='text-align:center; font-family:Arial;'>";
+
+    cout << "<h1>Welcome 🌍</h1>";
+    cout << "<p>This website is generated using C++</p>";
+
+    cout << "</body>";
+    cout << "</html>";
+
+    return 0;
+}
